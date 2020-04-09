102d8b4a0ab800aa1e6a4311bc6ecdbd163e0587
fullife32 <eithan.assouline6@gmail.com>
2020-04-09 17:56:57 +0200

    add key hook

--- mlx_test.c
+++ mlx_test.c
@@ -6,11 +6,19 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/09 16:11:43 by eassouli          #+#    #+#             */
-/*   Updated: 2020/04/09 17:18:42 by eassouli         ###   ########.fr       */
+/*   Updated: 2020/04/09 17:55:33 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raycasting.h"
+
+int	deal_key(int key, void *params)
+{
+	(void)key;
+	(void)params;
+	ft_putnbr_fd(key, 1);
+	return (OK);
+}
 
 int	mlx_test(t_res *res)
 {
@@ -19,7 +27,8 @@ int	mlx_test(t_res *res)
 
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, res->width, res->height, "WOA");
-	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF)
-;	mlx_loop(mlx_ptr);
+	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
+	mlx_key_hook(win_ptr, deal_key, (void *)0);
+	mlx_loop(mlx_ptr);
 	return (OK);
 }
