ec86126988ca3e3b400dfce663adb1bbbb47da0d
fullife32 <eithan.assouline6@gmail.com>
2020-04-09 18:13:21 +0200

    turn on pixel with keys

--- mlx_test.c
+++ mlx_test.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/09 16:11:43 by eassouli          #+#    #+#             */
-/*   Updated: 2020/04/09 18:02:31 by eassouli         ###   ########.fr       */
+/*   Updated: 2020/04/09 18:13:01 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 int	deal_key(int key, void *params)
 {
-	(void)key;
-	(void)params;
 	ft_putnbr_fd(key, 1);
+	mlx_pixel_put(((t_mlx *)params)->mlx_ptr, ((t_mlx *)params)->win_ptr, key, key, 0xFFFFFF);
 	return (OK);
 }
 
@@ -27,7 +26,7 @@ int	mlx_test(t_res *res)
 	mlx.mlx_ptr = mlx_init();
 	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, res->width, res->height, "WOA");
 	mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, 250, 250, 0xFFFFFF);
-	mlx_key_hook(mlx.win_ptr, deal_key, (void *)0);
+	mlx_key_hook(mlx.win_ptr, deal_key, &mlx);
 	mlx_loop(mlx.mlx_ptr);
 	return (OK);
 }
