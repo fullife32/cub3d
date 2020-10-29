6d99980e4dff2585fdd3ae06d0c29830e4d1294f
fullife32 <eithan.assouline6@gmail.com>
2020-10-29 18:44:12 +0100

    bonus

--- srcs/keys_move.c
+++ srcs/keys_move.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/25 15:05:04 by eassouli          #+#    #+#             */
-/*   Updated: 2020/10/29 18:39:56 by eassouli         ###   ########.fr       */
+/*   Updated: 2020/10/29 18:43:56 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@ void	key_front(t_a *a)
 	if (a->mov.mov[FW] == OK)
 	{
 		if (a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x + a->dir.x * a->plr.move_spd)] != '1'
-		&& a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x + a->dir.x * a->plr.move_spd)] != '1')
+		&& a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x + a->dir.x * a->plr.move_spd)] != '2')
 			a->plr.pos_x += a->dir.x * a->plr.move_spd;
 		if (a->map.map[(int)(a->plr.pos_y + a->dir.y * a->plr.move_spd)][(int)a->plr.pos_x] != '1'
-		&& a->map.map[(int)(a->plr.pos_y + a->dir.y * a->plr.move_spd)][(int)a->plr.pos_x] != '1')
+		&& a->map.map[(int)(a->plr.pos_y + a->dir.y * a->plr.move_spd)][(int)a->plr.pos_x] != '2')
 			a->plr.pos_y += a->dir.y * a->plr.move_spd;
 	}
 	if (a->mov.mov[BW] == OK)
 	{
 		if (a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x - a->dir.x * a->plr.move_spd)] != '1'
-		&& a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x - a->dir.x * a->plr.move_spd)] != '1')
+		&& a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x - a->dir.x * a->plr.move_spd)] != '2')
 			a->plr.pos_x -= a->dir.x * a->plr.move_spd;
 		if (a->map.map[(int)(a->plr.pos_y - a->dir.y * a->plr.move_spd)][(int)a->plr.pos_x] != '1'
-		&& a->map.map[(int)(a->plr.pos_y - a->dir.y * a->plr.move_spd)][(int)a->plr.pos_x] != '1')
+		&& a->map.map[(int)(a->plr.pos_y - a->dir.y * a->plr.move_spd)][(int)a->plr.pos_x] != '2')
 			a->plr.pos_y -= a->dir.y * a->plr.move_spd;
 	}
 }
@@ -40,19 +40,19 @@ void	key_side(t_a *a)
 	if (a->mov.mov[L] == OK)
 	{
 		if (a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x - a->dir.plane_x * a->plr.move_spd)] != '1'
-		&& a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x - a->dir.plane_x * a->plr.move_spd)] != '1')
+		&& a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x - a->dir.plane_x * a->plr.move_spd)] != '2')
 			a->plr.pos_x -= a->dir.plane_x * a->plr.move_spd;
 		if (a->map.map[(int)(a->plr.pos_y - a->dir.plane_y * a->plr.move_spd)][(int)a->plr.pos_x] != '1'
-		&& a->map.map[(int)(a->plr.pos_y - a->dir.plane_y * a->plr.move_spd)][(int)a->plr.pos_x] != '1')
+		&& a->map.map[(int)(a->plr.pos_y - a->dir.plane_y * a->plr.move_spd)][(int)a->plr.pos_x] != '2')
 			a->plr.pos_y -= a->dir.plane_y * a->plr.move_spd;
 	}
 	if (a->mov.mov[R] == OK)
 	{
 		if (a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x + a->dir.plane_x * a->plr.move_spd)] != '1'
-		&& a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x + a->dir.plane_x * a->plr.move_spd)] != '1')
+		&& a->map.map[(int)a->plr.pos_y][(int)(a->plr.pos_x + a->dir.plane_x * a->plr.move_spd)] != '2')
 			a->plr.pos_x += a->dir.plane_x * a->plr.move_spd;
 		if (a->map.map[(int)(a->plr.pos_y + a->dir.plane_y * a->plr.move_spd)][(int)a->plr.pos_x] != '1'
-		&& a->map.map[(int)(a->plr.pos_y + a->dir.plane_y * a->plr.move_spd)][(int)a->plr.pos_x] != '1')
+		&& a->map.map[(int)(a->plr.pos_y + a->dir.plane_y * a->plr.move_spd)][(int)a->plr.pos_x] != '2')
 			a->plr.pos_y += a->dir.plane_y * a->plr.move_spd;
 	}
 }
