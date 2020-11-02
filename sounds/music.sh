823771a97f247234e5b8d92af496529fccaaa63f
fullife32 <eithan.assouline6@gmail.com>
2020-11-02 17:13:08 +0100

    music

--- sounds/music.sh
+++ sounds/music.sh
@@ -6,14 +6,14 @@
 #    By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/10/09 14:59:10 by eassouli          #+#    #+#              #
-#    Updated: 2020/11/02 17:09:37 by eassouli         ###   ########.fr        #
+#    Updated: 2020/11/02 17:13:06 by eassouli         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
 while pidof ./cub3D > /dev/null == true ; do
-	ret = aplay --quiet ./sounds/bfg10k.wav ;
+	let "ret = aplay --quiet ./sounds/bfg10k.wav" ;
 	if [ ret != 1 ] ; then
 		break
 	fi
