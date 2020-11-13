6ce27d0ddf94598c88e4175d8648fb2dda4abb51
fullife32 <eithan.assouline6@gmail.com>
2020-11-13 14:47:42 +0100

    errors

--- srcs/errors.c
+++ srcs/errors.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 18:19:43 by eassouli          #+#    #+#             */
-/*   Updated: 2020/10/14 18:20:08 by eassouli         ###   ########.fr       */
+/*   Updated: 2020/11/13 14:46:26 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,46 @@
 #include "raycast.h"
 #include "stack.h"
 
+int		destroy(t_a *a)
+{
+	system("kill -9 $(pidof aplay) > /dev/null");
+	mlx_destroy_window(a->mlx.ptr, a->mlx.win);
+	free_map(a->map.map);
+	exit(OK); // Ne pas juste exit mais return error
+}
+
+int	fp(int fd, t_txr *txr)
+{
+	if (txr->north)
+		free(txr->north);
+	if (txr->south)
+		free(txr->south);
+	if (txr->west)
+		free(txr->west);
+	if (txr->east)
+		free(txr->east);
+	if (txr->sprite)
+		free(txr->sprite);
+	if (txr->floor)
+		free(txr->floor);
+	if (txr->ceiling)
+		free(txr->ceiling);
+	return (ERR);
+}
+
+int	error(int fd, int error, t_a *a)
+{
+	if (error != ERR)
+		write(1, "Error\n", 7);
+	if (error == MISSING_CUB_FILE)
+		write(1, ".cub file not found\n", 21);
+	else if (error == NOT_CUB_FILE)
+		write(1, "The last argument must be a .cub file\n", 38);
+	else if (error == MLX_INIT_FAIL)
+		write(1, "Initialization of MLX server failed\n", 37);
+	else if (error == MUSIC_FILE_FAIL)
+		write(1, "Music file name/path is incorrect\n", 35);
+	if (error == ERR)
+		fp(fd, &a->txr);
+	return (ERR);
+}
--- srcs/raycast.c
+++ srcs/raycast.c
@@ -6,20 +6,12 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/25 15:10:31 by eassouli          #+#    #+#             */
-/*   Updated: 2020/11/02 17:17:21 by eassouli         ###   ########.fr       */
+/*   Updated: 2020/11/13 14:42:49 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 #include "raycast.h"
-
-int		destroy(t_a *a)
-{
-	system("kill -9 $(pidof aplay)");
-	mlx_destroy_window(a->mlx.ptr, a->mlx.win);
-	free_map(a->map.map);
-	exit(OK); // Ne pas juste exit mais return error
-}
 
 int		rc_loop(t_a *a)
 {
@@ -55,7 +47,8 @@ int		image_loader(t_a *a)
 		return(ERR);
 	if (sprite_init(a) == ERR)
 		return (ERR);
-	a->mlx.win = mlx_new_window(a->mlx.ptr, a->res.w, a->res.h, "cub3D");
+	if (a->bmp.fd == 0)
+		a->mlx.win = mlx_new_window(a->mlx.ptr, a->res.w, a->res.h, "cub3D");
 	a->img.img_ptr = mlx_new_image(a->mlx.ptr, a->res.w, a->res.h);
 	a->img.img = mlx_get_data_addr(a->img.img_ptr, &a->img.bpp, &a->img.size_l, &a->img.endian);
 	return (OK);
